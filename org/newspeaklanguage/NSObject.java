01ffb2f65ac06aea29e41e01af10f345d95809db
Keegan <kjrandez@gmail.com>
2017-04-23 12:39:58 -0400

    Moved Generator into JNI

--- org/newspeaklanguage/NSObject.java
+++ org/newspeaklanguage/NSObject.java
@@ -4,31 +4,60 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class NSObject
 {
-	public long tag;
-	public ConcurrentLinkedQueue<NSMessage> queue;
+	private long tag;
+	private ConcurrentLinkedQueue<NSMessage> queue;
+	
+	/* Only Newspeak can provide the queue needed for a registered NSObject. */
+	public NSObject(long tag, ConcurrentLinkedQueue<NSMessage> queue) {
+		this.tag = tag;
+		this.queue = queue;
+	}
 	
 	/* Non-blocking send. */
 	public void send(String selector, Object... args) {
-		queue.add(new NSMessage(tag, selector, args));
+		queue.add(new NSMessage(false, tag, selector, args));
 	}
 	
-	/* Blocking send. */
-	public void sync(String selector, Object... args) throws DoesNotUnderstand
+	/* Blocking send, yields a result. */
+	public Object sync(String selector, Object... args)
 	{
+		NSMessage message = new NSMessage(true, tag, selector, args);
+		queue.add(message);
 		
+		synchronized(message) {
+			while(message.pending) {
+				try { message.wait(); }
+				catch (InterruptedException e) {}
+			}
+		}
+		
+		return message.result;
+	}
+
+	/* Convenience, if user knows result is an NSOBject. */
+	public NSObject syncNS(String selector, Object... args)
+	{
+		return (NSObject)this.sync(selector, args);
 	}
 	
-	/* Blocking send that yields a result. */
-	public Object get(String selector, Object... args) throws DoesNotUnderstand
-	{
-		return null;
-	}
+	@SuppressWarnings("unused")
 	
-	/* Blocking send that yields an NSObject registered with the Java Process. */
-	public NSObject getNSObj(String selector, Object... args) throws DoesNotUnderstand
+	private class NSMessage
 	{
-		return null;
+		public long tag;
+		public String selector; 
+		public Object[] args;
+		
+		public boolean pending;
+		public Object result;
+		
+		public NSMessage(boolean pending, long tag, String selector, Object...args) {
+			this.tag = tag;
+			this.selector = selector;
+			this.args = args;
+			
+			this.pending = pending;
+			this.result = null;
+		}
 	}
 }
-
-
